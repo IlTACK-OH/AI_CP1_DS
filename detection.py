8464073cd7579615b383ab8acd90d87aadab551f
ILTACK-OH <dlfxor0329@khu.ac.kr>
2023-03-20 23:48:09 +0900

    1차 업데이트

---
+++ detection.py
@@ -0,0 +1,54 @@
+import argparse
+import os
+import sys
+import time
+from pathlib import Path
+
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+import cv2
+from utils.video import video_detect
+from utils.image import img_predict
+def run(
+    onnx_path = ROOT / 'models/best.onnx',
+    source = ROOT / 'data',
+    output = ROOT/ 'results',
+    conf = 0.4
+):
+    start = time.time()
+    
+    # yolov5 모델 로드
+    net = cv2.dnn.readNet(onnx_path)
+
+    for filename in os.listdir(source):   
+        file_path = os.path.join(source, filename)
+        output_path = os.path.join(output, filename)
+        
+        if file_path.endswith(".mp4"):
+            video_detect(net = net, video_path = file_path, output_path=output_path, conf = conf)
+            pass
+        elif file_path.endswith(".jpg") or file_path.endswith(".png"):
+            img_predict(net = net, image_path = file_path, output_path=output_path, conf = conf)
+        
+    print(f"Wall time: {int((time.time()-start)//60)}min {(int(time.time()-start)%60)}s")
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--onnx_path', type=str, default=ROOT / 'models/best.onnx')
+    parser.add_argument('--source', type=str, default= ROOT / 'data')
+    parser.add_argument('--output', type=str, default= ROOT/ 'results')
+    parser.add_argument('--conf', type=float, default= 0.25)
+    args = parser.parse_args()
+    return args
+
+def main(args):
+    run(**vars(args))
+    
+if __name__ == '__main__':
+    args = parse_opt()
+    main(args)
---
+++ utils/video.py
@@ -0,0 +1,120 @@
+import os
+import cv2
+import numpy as np
+import time
+
+# Constants.
+INPUT_WIDTH = 640
+INPUT_HEIGHT = 640
+SCORE_THRESHOLD = 0.5
+NMS_THRESHOLD = 0.45
+
+# Text parameters.
+FONT_FACE = cv2.FONT_HERSHEY_SIMPLEX
+FONT_SCALE = 1.2
+THICKNESS = 2
+ 
+# Colors.
+BLUE   = (255,178,50)
+RED = (0, 0, 255)
+YELLOW = (0, 255, 255)
+WHITE = (255,255,255)
+
+classes = ["illegal", "normal"]
+
+def video_detect(net, video_path, output_path, conf):
+    CONFIDENCE_THRESHOLD = conf
+    
+    # Load image or video using OpenCV
+    if os.path.isfile(video_path):	# 해당 파일이 있는지 확인
+        # 영상 객체(파일) 가져오기
+        cap = cv2.VideoCapture(video_path)
+    else:
+        print("파일이 존재하지 않습니다.")
+    
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_path, fourcc, fps, frame_size)
+    
+    start = time.time()
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        img = frame.copy()
+        
+        # Create a 4D blob from a frame.
+        blob = cv2.dnn.blobFromImage(frame, 1/255,  (INPUT_WIDTH, INPUT_HEIGHT), [0,0,0], 1, crop=False)
+        
+        # Sets the input to the network.
+        net.setInput(blob)
+    
+        # Run the forward pass to get output of the output layers.
+        outputs = net.forward(net.getUnconnectedOutLayersNames())
+        
+        
+       # Lists to hold respective values while unwrapping.
+        class_ids = []
+        confidences = []
+        boxes = []
+        # Rows.
+        rows = outputs[0].shape[1]
+        image_height, image_width = img.shape[:2]
+        # Resizing factor.
+        x_factor = image_width / INPUT_WIDTH
+        y_factor =  image_height / INPUT_HEIGHT
+        # Iterate through detections.
+        for r in range(rows):
+            row = outputs[0][0][r]
+            confidence = row[4]
+            # Discard bad detections and continue.
+            if confidence >= CONFIDENCE_THRESHOLD:
+                    classes_scores = row[5:]
+                    # Get the index of max class score.
+                    class_id = np.argmax(classes_scores)
+                    #  Continue if the class score is above threshold.
+                    if (classes_scores[class_id] > SCORE_THRESHOLD):
+                        confidences.append(confidence)
+                        class_ids.append(class_id)
+                        cx, cy, w, h = row[0], row[1], row[2], row[3]
+                        left = int((cx - w/2) * x_factor)
+                        top = int((cy - h/2) * y_factor)
+                        width = int(w * x_factor)
+                        height = int(h * y_factor)
+                        box = np.array([left, top, width, height])
+                        boxes.append(box)
+        
+        # Perform non maximum suppression to eliminate redundant, overlapping boxes with lower confidences.
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+        for i in indices:
+                box = boxes[i]
+                left = box[0]
+                top = box[1]
+                width = box[2]
+                height = box[3]
+                    
+                # Class label.                      
+                label = "{}:{:.2f}".format(classes[class_ids[i]], confidences[i])
+                
+                # Get text size.
+                text_size = cv2.getTextSize(label, FONT_FACE, FONT_SCALE, THICKNESS)
+                dim, baseline = text_size[0], text_size[1]
+                
+                # Draw bounding box.
+                if classes[class_ids[i]] == "illegal":
+                    cv2.rectangle(img, (left, top), (left + width, top + height), RED, 3*THICKNESS)
+                    cv2.rectangle(img, (left,top), (left + dim[0], top + dim[1] + baseline), RED, cv2.FILLED)
+                    cv2.putText(img, label, (left, top + dim[1]), FONT_FACE, FONT_SCALE, WHITE, THICKNESS, cv2.LINE_AA)
+                elif classes[class_ids[i]] == "normal":
+                    cv2.rectangle(img, (left, top), (left + width, top + height), BLUE, 3*THICKNESS)
+                    cv2.rectangle(img, (left,top), (left + dim[0], top + dim[1] + baseline), BLUE, cv2.FILLED)
+                    cv2.putText(img, label, (left, top + dim[1]), FONT_FACE, FONT_SCALE, WHITE, THICKNESS, cv2.LINE_AA)        
+        # Write frame to output video
+        out.write(img)
+        print(f"\rObject detection in progress❗️ Time taken:{round(time.time() - start, 2)}",end="")
+    # Release resources
+    cap.release()
+    out.release()
+    print(f"    ⭐️complete!!!⭐️ Video is store in{output_path}")
